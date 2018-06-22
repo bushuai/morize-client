7c95234cbcf908d4057cebd0d13378263d27c9e2
bushuai <ibushuai@gmail.com>
2018-06-22 18:47:19 +0800

    chore: add leancloud

---
+++ src/services/leancloudService.js
@@ -0,0 +1,21 @@
+// storage service
+import AV from '../libs/av-weapp'
+// real-time service
+import { Realtime, TextMessage } from '../libs/realtime-weapp'
+import { LEANCLOUD_APPID as appId, LEANCLOUD_APPKEY as appKey } from '../constants/KEYS'
+// for LiveQuery use import AV from 'leancloud-storage/live-query'
+const { Query, User } = AV
+AV.init({ appId, appKey })
+
+const Morize = AV.Object.extend('morize')
+const DB = new Morize()
+
+export {
+  DB,
+  Query,
+  User,
+  Realtime,
+  TextMessage
+}
+
+export default AV
